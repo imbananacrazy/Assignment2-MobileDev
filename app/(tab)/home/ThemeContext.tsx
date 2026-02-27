32c88f1353e57c16a0c4a7ce832514c14c63c557
Dmpoke66 <daniel.maliczewski@edu.sait.ca>
2026-02-27 16:12:46 -0700

    Implement ThemeContext for theme management

---
+++ app/(tab)/home/ThemeContext.tsx
@@ -0,0 +1,28 @@
+import React, { createContext, useContext, useState } from "react";
+
+type ThemeType = "light" | "dark";
+interface ThemeContextType {
+  theme: ThemeType;
+  toggleTheme: () => void;
+}
+
+const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
+
+export const ThemeProvider = ({ children}: any) => {
+  const [theme, settheme] = useState<ThemeType>("dark");
+
+  const toggleTheme = () => {
+    setTheme ((prev) => (prev === "dark" ? "light" :"dark"));
+  };
+  return (
+    <ThemeContext.Provider value ={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export const useTheme = () => {
+  const context = useContext(ThemeContext);
+  if (!context) throw new Error("useTheme must be used inside ThemeProvider");
+  return context;
+};
